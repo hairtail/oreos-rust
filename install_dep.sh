38e6c23a864e688ccb0dc3a301a60a21c930a491
hairtail <zhaoxinlong@web3.com>
2023-04-12 20:31:26 +0800

    Update dep to ironfish v0.1.75

--- install_dep.sh
+++ install_dep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ ! -d "./ironfish" ];then
-    git clone -b rust-v0.1.74 https://github.com/hairtail/ironfish.git --depth 1
+    git clone -b oreorust-0.1.75 https://github.com/hairtail/ironfish.git --depth 1
 fi
